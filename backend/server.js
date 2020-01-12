8b535cab64397a33000834e100fb025ce9aa6acf
Michael DeMarco <35978975+michaelfromyeg@users.noreply.github.com>
2020-01-11 17:30:01 -0800

    Basic file merge

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,15 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
+console.log(require('dotenv').config())
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 const uri = process.env.ATLAS_URI;
+
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true } );
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -21,13 +22,11 @@ const courseRouter = require('./routes/course');
 const lectureRouter = require('./routes/lecture');
 const noteRouter = require('./routes/note');
 const userRouter = require('./routes/user');
-const fileRouter = require('./routes/file');
 
 app.use('/course', courseRouter);
 app.use('/lecture', lectureRouter);
 app.use('/note', noteRouter);
 app.use('/user', userRouter);
-app.use('/file', fileRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
